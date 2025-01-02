053fd4326a600b22c1deb7c09964aef10fda33e9
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-02 21:43:44 +0200

    Polymorphism Method Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace Demo
+﻿using Demo.inheritance;
+
+namespace Demo
 {
     internal class Program
     {
+      
+        static int Sum (int X,int Y)
+        {
+            return X + Y;    
+        }
+
+        static int Sum(int X, int Y, int Z)
+        {
+            return X + Y +Z ;
+        }
+
+        static double Sum(double X, double Y)
+        {
+            return X + Y;
+        }
+
+        static double Sum(int X, double Y)
+        {
+            return X + Y;
+        }
+
+        static double Sum(double X, int Y)
+        {
+            return X + Y;
+        }
         static void Main(string[] args)
         {
+            //inheritance
+
+            // Parent parent = new Parent(1, 2);
+            // Console.WriteLine(parent);
+
+            // parent.Fun01();
+            //parent.Fun02();
+
+            // Child child=new Child(1,2,3);
+            //// Console.WriteLine(child);
+            // child.Fun01();
+            // child.Fun02();
+
+            Sum(1, 2);
+            Sum(1.3, 4.3);
            
         }
     }
